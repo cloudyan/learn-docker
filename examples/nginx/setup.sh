1e7950f80ce97ae65d73be2f47b0ffda7812740d
cloudyan <1395093509@qq.com>
2020-07-08 15:14:23 +0800

    feat: update nginx

--- examples/nginx/setup.sh
+++ examples/nginx/setup.sh
@@ -1,3 +1,12 @@
+#!/bin/sh
 
 
-docker run --name some-nginx -v /some/content:/usr/share/nginx/html:ro -d nginx
+docker run -it \
+  --name my-nginx \
+  -p 5500:80 \
+  -v /data/apps/h5:/data/apps/h5 \
+  -v /config/nginx/conf.d:/etc/nginx/conf.d \
+  -d \
+  nginx:alpine
+
+# --rm
